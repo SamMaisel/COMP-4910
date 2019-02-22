1b2e72896486fd06c527f606743d730431af990f
paperwarior <46761104+paperwarior@users.noreply.github.com>
2019-02-22 13:05:31 -0600

    Add files via upload

---
+++ Assignment 2.sql
@@ -0,0 +1,12 @@
+use employees;
+SELECT gender,
+min(salary),max(salary),avg(salary),
+count(salary)
+from employees as e, salaries as s
+where (e.emp_no=s.emp_no)
+group by gender;
+select count(emp_no) from dept_emp where ((from_date<'1991-03-07') and (to_date>'1985-01-01'))
+and dept_no='d007';
+select count(distinct emp_no) from salaries;
+select emp_no, avg(salary) from salaries group by emp_no;
+select count(emp_no) from titles where (title='senior staff' or title='staff')
